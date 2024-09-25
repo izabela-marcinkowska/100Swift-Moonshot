4efeb14bac8b12f5d89d8db8abe75521e96c528c
Devy <izabela.marcinkowska@outlook.com>
2024-09-25 12:54:03 +0200

    First commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Izabela Marcinkowska on 2024-09-25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
