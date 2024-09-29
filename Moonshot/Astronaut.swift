b6dfc67d0ef908ca776a9d9dea5cd396bf19d497
Devy <izabela.marcinkowska@outlook.com>
2024-09-29 20:43:20 +0200

    feat: Decode astronauts JSON failes

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Izabela Marcinkowska on 2024-09-29.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts = Bundle.main.decode("astronauts.json")
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+                    Text(String(astronauts.count))
+
         }
-        .padding()
+
     }
 }
 
