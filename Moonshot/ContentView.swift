2f9cd677f48b3fcc2015fe9bbf69b453a2f49175
Devy <izabela.marcinkowska@outlook.com>
2024-10-01 11:06:03 +0200

    feat: Show missions with dates in grid

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -15,7 +15,29 @@ struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     var body: some View {
         NavigationStack {
-            
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
             .navigationTitle("Moonshot")
         }
 
